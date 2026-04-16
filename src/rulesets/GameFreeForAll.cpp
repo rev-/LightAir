2575cf652b33f78d00b4b15e1649bd461e9215b3
rev- <piero.lovato@gmail.com>
2026-04-16 11:47:16 +0200

    Change UI event from 'Lit' to 'Taken'

--- src/rulesets/GameFreeForAll.cpp
+++ src/rulesets/GameFreeForAll.cpp
@@ -123,7 +123,7 @@ static const DirectRadioRule directRadioRules[] = {
 
 static void onReplyTaken(const RadioPacket&, const RadioPacket&,
                          LightAir_DisplayCtrl&, GameOutput& out) {
-    out.ui.trigger(LightAir_UICtrl::UIEvent::Lit);
+    out.ui.trigger(LightAir_UICtrl::UIEvent::Taken);
 }
 
 static void onReplyShone(const RadioPacket&, const RadioPacket&,
